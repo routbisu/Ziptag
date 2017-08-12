a7f6e03719e7d37ead37e89f8353697e2feb458d
Biswaranjan Rout <Biswaranjan.rout1989@gmail.com>
2017-08-12 17:07:08 +0530

    Changed folder structure

---
+++ public/admin/src/assets/js/site.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Hide loader overlay div
+var hideLoader = function() {
+  $('.loader-overlay').hide();
+}
+
+var showLoader = function() {
+  $('.loader-overlay').show();
+}
+
+var loaderTest = function() {
+  console.log("Yes");
+  showLoader();
+  setTimeout(function() {
+    hideLoader();
+  }, 6000);
+}
+
+// Handle side nav bar for small screens
+$(document).ready(function() {
+
+  // Hamburger menu
+  $("#sidebar-hamburger").click(function(event) {
+    $("#side-nav-bar").addClass('open-side-nav');
+    event.stopPropagation();
+  });
+
+  $(window).click(function() {
+    $("#side-nav-bar").removeClass('open-side-nav');
+  });
+
+  $('#side-nav-bar').click(function(event){
+      event.stopPropagation();
+  });
+
+  // Submenu - Side nav
+  $('#master-data-sub-menu').hide();
+
+  $('#master-data-menu').hover(function() {
+    $('#master-data-menu .right-chevron').addClass('right-chevron-hovered');
+  }, function() {
+    $('#master-data-menu .right-chevron').removeClass('right-chevron-hovered');
+  });
+
+  $('#master-data-menu').click(function () {
+    $('#master-data-sub-menu').slideToggle();
+  });
+
+  hideLoader();
+
+  $('#loaderTest').click(loaderTest);
+
+});
---
+++ public/admin/src/main.ts
@@ -0,0 +1,11 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { LoginModule } from './app/components/login/login.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(LoginModule);
