afe4db261999af9064ad078a7f5263af6469dba9
Biswaranjan Rout <Biswaranjan.rout1989@gmail.com>
2017-08-12 23:42:16 +0530

    Final structure for angular 4 App

--- public/admin/src/app/components/login/login.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './login.component.html'
-})
-export class LoginComponent {
-  title = 'app';
-}
--- public/admin/src/main.ts
+++ public/admin/src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { LoginModule } from './app/components/login/login.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(LoginModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
